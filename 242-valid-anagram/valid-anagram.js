b160ba82dd89aae2671072dccb396efad84c5abb
Saniya Talikoti <saniyatalikoti1311@gmail.com>
2026-08-07 15:03:08 +0530

    Time: 16 ms (46.27%) | Memory: 60 MB (21.54%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length!==t.length)
+    return false;
+
+let map={};
+for(let char of s)
+{
+    if(map[char])
+    {
+        map[char]=map[char]+1;
+    }
+    else{
+        map[char]=1;
+    }
+}
+for(let char of t)
+{
+    if(!map[char]) return false;
+    map[char]--;
+}
+return true;
+};
