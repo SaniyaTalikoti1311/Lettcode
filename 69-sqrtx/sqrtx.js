18baaee6aa02d94a3a74af7f04303c47843c5c25
Saniya Talikoti <saniyatalikoti1311@gmail.com>
2026-08-07 20:06:21 +0530

    Time: 1 ms (74.90%) | Memory: 56.8 MB (27.47%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let left=0;
+    let right=x;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(mid*mid===x){
+            return mid;
+        }
+        if(mid*mid>x)
+        {
+            right=mid-1;
+        }
+        else{
+            left=mid+1;
+        }
+        
+    }
+    return right;
+};
